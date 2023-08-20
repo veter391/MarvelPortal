3fbbea4bda68488a22170f2a4394fd2be68207d6
veter391 <nazarcova391@gmail.com>
2023-08-20 21:28:33 +0200

    change randomChar, add spinner and check Errors

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,10 @@
+import img from './error.gif';
+
+const ErrorMessage = () => {
+    return (
+        // <img src={process.env.PUBLIC_URL + '/error.gif'} alt="error" /> exemple how tu use info from global folder
+        <img src={img} alt="error" style={{display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin: '0 auto'}}/>
+    )
+}
+
+export default ErrorMessage;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,8 +1,9 @@
 import { Component } from 'react';
 import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage'
 
 import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component{
@@ -13,16 +14,31 @@ class RandomChar extends Component{
     }
 
     state = {
-        char:{}
+        // create object char because state can be other info, errors, spinner etc...
+        // create object char with info about character
+        char: {},
+        loading: true,
+        error: false
     }
 
     // if want to work with a class you need to create an instance of it!!! 
     marvelService = new MarvelService();
 
-    // change state.char and add new obj
-    onCharLoaded = char => this.setState({char});
+    // change info when is loaded
+    onCharLoaded = char => this.setState({
+        // change state.char and add new obj
+        char, 
+        // change loading state when char is loaded
+        loading: false
+    });
 
-
+    onError = () => {
+        this.setState({
+            // change loading state when char is loaded
+            loading: false,
+            error: true
+        })
+    }
 
     updateChar = () => {
         // generate random id for a char
@@ -31,33 +47,42 @@ class RandomChar extends Component{
         // get character info and change the state
         this.marvelService
             .getCharacter(id)
-            // .then(this.onCharLoaded) is the same .then(res => this.setState(res))
-            // if you call a function this function works with all elements in turn
+            /* 
+            .then(this.onCharLoaded) is the same .then(res => this.setState(res))
+            if you call a function this function works with all elements in turn
+            */
+           
+            
+            // if info is loadded correctly
             .then(this.onCharLoaded)
+            // if server return error
+            .catch(this.onError);
     }
 
 
     render() {
-        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
+        /* 
+        destructuritzation of object char inside state.
+        const {char: {name, description, thumbnail, homepage, wiki}, loading} = this.state;
+        */
+
+
+        // get ellements from state
+        const {char, loading, error} = this.state,
+        // if exist error return error
+            errorMessage = error ? <ErrorMessage/> : null,
+        // if wait info from server return spinner
+            spinner = loading ? <Spinner/> : null,
+        // if doesn't exist error and spinner is null return content
+            content = !(loading || error) ? <View char={char}/> : null
+
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {description}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {/* if loading true return animated spinner */}
+                {/* {loading ? <Spinner/> : <View char={char}/>} */}
+                { errorMessage }
+                { spinner }
+                { content }
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -74,6 +99,32 @@ class RandomChar extends Component{
             </div>
         )
     }
+}
+
+// create new componet with dinamic block, check it and generate spinner or info
+const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki} = char;
+
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {/* check if exist descr and if not add custom text */}
+                    {description ? description : 'Not info about this character !'}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default RandomChar;
